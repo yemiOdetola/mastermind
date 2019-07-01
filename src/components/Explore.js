acbea36e2e429fbc3354fe4bcbd2019e8718d643
blarcmann <yemiotola@outlook.com>
2019-07-01 15:58:29 +0100

    Welcome page WIP

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../css/index.css';
+import '../css/explore.css';
 
 export default class Explore extends React.Component {
     constructor(props) {
@@ -11,23 +11,17 @@ export default class Explore extends React.Component {
 
     render() {
         return (
-            <div className="explore">
-                ExploreME!
-                <div className="svg">
-                    <svg xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="741" height="177" viewBox="0, 0, 741, 177">
-                        <g id="Background">
-                            <rect x="0" y="0" width="741" height="177" fill="#000000" fillOpacity="0" />
-                        </g>
-                        <defs>
-                            <linearGradient id="Gradient_1" gradientUnits="userSpaceOnUse" x1="245.576" y1="215.479" x2="462.478" y2="-160.207" gradientTransform="matrix(1, 0, 0, 1, 0, 0)">
-                                <stop offset="0.4" stopColor="#48CFFC" />
-                                <stop offset="1" stopColor="#FFFFFF" stopOpacity="0" />
-                            </linearGradient>
-                        </defs>
-                        <g id="Layer_1">
-                            <path d="M0,0 L147.803,156.12 C163.831,173.055 188.213,179.292 210.402,172.072 L739.962,0 L0,0 z" fill="url(#Gradient_1)" opacity="0.1" />
-                        </g>
-                    </svg>
+            <div className="body-cover">
+                <div className="contain">
+                    <div className="loose-cover">
+                        <div className="welcome-title">
+                            Heyy! <span role="img" aria-label="waving"> &#x1F44B;</span>, would you like to
+                        </div>
+                        <div className="actions">
+                            <button className="action primary">Play a Game</button>
+                            <button className="action secondary">How to Play</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
